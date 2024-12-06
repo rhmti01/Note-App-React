257b661734dde0ee1000ccc8c6bf7fb0b0795e0a
rhmti01 <rahmati.business@gmail.com>
2024-12-07 00:34:35 +0330

    app structure and tailwindcss added

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "/src/css/App.css";
+function App() {
+  return (
+    <div className="container" >
+      <div className="note-header">note header</div>
+      <div className="note-app">
+        <div className="add-new-note">add new note</div>
+        <div className="note-container">notes</div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
