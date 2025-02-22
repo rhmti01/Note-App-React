b0bc70ff5104db4e906648e264fe169e8378a6f7
rhmti01 <rahmati.business@gmail.com>
2025-02-22 23:32:25 +0330

    update edit note (modalOnEdit)

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,103 @@
 import { useState } from "react";
 import AddNewNote from "./components/AddNewNote.jsx";
 import NoteList from "./components/NoteList.jsx";
-import NoteStatus from "./components/NoteStatus.jsx"
+import NoteStatus from "./components/NoteStatus.jsx";
 import NoteHeader from "./components/NoteHeader.jsx";
+import ModalOnEdit from "./components/ModalOnEdit.jsx";
 import "/src/index.css";
 import "/src/App.css";
 
-
 function App() {
-
-  // used states
   const [sortBy, setSortBy] = useState(() => {
-    const savedSortBy = localStorage.getItem("sortBy")
-    return savedSortBy ? savedSortBy : "latest"
-  })
+    const savedSortBy = localStorage.getItem("sortBy");
+    return savedSortBy ? savedSortBy : "latest";
+  });
+
   const [notes, setNotes] = useState(() => {
-    const savedNotes = localStorage.getItem('notes');
+    const savedNotes = localStorage.getItem("notes");
     return savedNotes ? JSON.parse(savedNotes) : [];
   });
-  const [editModal, setModal] = useState(false)
-  const [noteToEdit, setNoteToEdit] = useState({})
 
-  // push new notes to the notes state
+  const [editModal, setModal] = useState(false);
+  const [selectedNote, setSelectedNote] = useState(null); 
+
   const handleAddNote = (newNote) => {
     setNotes((prevNotes) => {
       const updatedNotes = [...prevNotes, newNote];
-      localStorage.setItem('notes', JSON.stringify(updatedNotes));
+      localStorage.setItem("notes", JSON.stringify(updatedNotes));
       return updatedNotes;
     });
   };
 
-  // remove notes  based on object filter
   const handleRemoveNote = (removedNoteId) => {
     setNotes((prevNotes) => {
       const updatedNotes = prevNotes.filter((cmp) => cmp.id !== removedNoteId);
-      localStorage.setItem('notes', JSON.stringify(updatedNotes));
+      localStorage.setItem("notes", JSON.stringify(updatedNotes));
       return updatedNotes;
     });
   };
 
-  // edit note by new value
-  const handleEditNote = (editableNoteId) => {
-    // console.log(editableNoteId);
-    const editNote = notes.find((note) => note.id == editableNoteId)
-    if (editNote && !editNote.completed) {
-      setNoteToEdit(editNote);
-      setModal(true);
-    }
-  }
-
-  // toggle notes completion  
   const handleCompleteNote = (e) => {
     const noteId = Number(e.target.value);
     setNotes((prevNotes) => {
       const updatedNotes = prevNotes.map((note) =>
-        note.id === noteId ? { ...note, completed: !note.completed } : note);
-      localStorage.setItem('notes', JSON.stringify(updatedNotes));
+        note.id === noteId ? { ...note, completed: !note.completed } : note
+      );
+      localStorage.setItem("notes", JSON.stringify(updatedNotes));
       return updatedNotes;
     });
   };
 
-  // sort notes based on sortBy
   const handleSortNotes = () => {
-    let sortedNotes = [...notes]
+    let sortedNotes = [...notes];
 
     switch (sortBy) {
       case "latest":
-        sortedNotes = sortedNotes.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+        sortedNotes = sortedNotes.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
         break;
       case "earliest":
-        sortedNotes = sortedNotes.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+        sortedNotes = sortedNotes.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
         break;
       case "completed":
-        sortedNotes = sortedNotes.sort((a, b) => Number(b.completed) - (Number(a.completed)))
+        sortedNotes = sortedNotes.sort((a, b) => Number(b.completed) - Number(a.completed));
     }
 
-    return sortedNotes
-  }
+    return sortedNotes;
+  };
 
-  // save selected sort
   const handleSortChange = (e) => {
-    const newSortBy = e.target.value
-    setSortBy(newSortBy)
-    localStorage.setItem("sortBy", newSortBy)
-  }
+    const newSortBy = e.target.value;
+    setSortBy(newSortBy);
+    localStorage.setItem("sortBy", newSortBy);
+  };
 
-  const handleEdit = (noteId) => {
-    const titleValue = document.querySelector("#title").value
-    const descriptionValue = document.querySelector("#description").value
-    // console.log(titleValue, descriptionValue);
+  const handleEditNote = (editableNoteId) => {
+    const editNote = notes.find((note) => note.id === editableNoteId);
+    if (editNote && !editNote.completed) {
+      setSelectedNote(editNote); 
+      setModal(true);
+    }
+  };
+
+  const recordEditNote = (noteId, updatedTitle, updatedDescription) => {
     setNotes((prevNotes) => {
       const updatedNotes = prevNotes.map((note) =>
-        note.id === noteId ? { ...note, title:titleValue , description : descriptionValue } : note);
-      localStorage.setItem('notes', JSON.stringify(updatedNotes));
+        note.id === noteId ? { ...note, title: updatedTitle, description: updatedDescription } : note
+      );
+      localStorage.setItem("notes", JSON.stringify(updatedNotes));
       return updatedNotes;
     });
-    setModal(!editModal)
-  }
+
+    setModal(false);
+    setSelectedNote(null); 
+  };
 
   return (
     <div className=" w-full 2xl:max-w-[1280px] xl:max-w-[1100px]  flex items-center flex-col xg:pb-24 mm:pb-20 ss:pb-10 " >
-      <ModalOnEdit note={noteToEdit} editModal={editModal} handleEdit={handleEdit} />
-      <NoteHeader
-        sortBy={sortBy}
-        onSortNotes={handleSortChange} />
+      {editModal && selectedNote && (
+        <ModalOnEdit note={selectedNote} setModal={setModal} recordEditNote={recordEditNote} />
+      )}
+      <NoteHeader sortBy={sortBy} onSortNotes={handleSortChange} />
       <div className=" flex xx:items-start ss:items-center xx:gap-y-0 gap-y-9 justify-evenly xx:flex-row ss:flex-col  w-full bg-yellow-50/ 2xl:mt-2 xl:mt-1  ">
         <AddNewNote onAddNote={handleAddNote} />
         <div className=" xg:basis-[47%] xx:basis-[50%] xx:w-full  mm:w-[470px] ss:w-[90%] bg-slate-30 w-full my-3  " >
@@ -112,58 +106,12 @@ function App() {
             sortedNotes={handleSortNotes()}
             onRemoveNote={handleRemoveNote}
             onCompleteNote={handleCompleteNote}
-            onEditNote={handleEditNote} />
+            onEditNote={handleEditNote}
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
-
-
-
-
-
-
-
-//  modal on the webpage with javascript  
-function ModalOnEdit({ note, editModal, handleEdit }) {
-  return (
-    <div className={`   ${editModal ? "block" : "hidden"}   `}  >
-      <div onClick={() => handleEdit(note.id)} class="  cursor-pointer  inset-0 absolute z-20 w-full h-screen bg-stone-900/40 backdrop-blur-md  "></div>
-      <div class=" scale-[83%]  z-40 absolute top-1/2  left-1/2 -translate-y-1/2 -translate-x-1/2    ">
-        <div
-          class="block border-2 border-stone-200  font-inter bg-white px-3 py-3 w-[330px] md:py-4 md:px-3  md:justify-between  md:w-[420px]  rounded-2xl ">
-          <div class=" flex items-center justify-between w-full py-2 bg-slate-400/  ">
-            <div></div>
-            <h2 class=" font-semibold text-[25px]  md:text-[27px] text-slate-900 ">Edit Todo</h2>
-            <svg onClick={() => handleEdit(note.id)} class=" cursor-pointer w-6 h-6 md:w-7 md:h-7 stroke-red-500  cancelBtn  " xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 24 24" stroke-width="4" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-          </div>
-          <span class="block w-[97%] mx-auto h-[2px] bg-slate-200 mt-1 md:mt-3 "></span>
-          <h1 class=" font-semibold text-[23.5px] md:text-[25px] text-slate-700 text-left mt-7 md:mt-12 ml-5  ss:ml-4  ">Title</h1>
-          <div class=" flex justify-center " >
-            <input id="title" autofocus="autofocus" defaultValue={note.title}
-              class=" border-2  border-stone-300 mx-auto w-[280px] ss:w-[290px] md:w-[360px] px-2 md:px-4 my-3 py-2.5 rounded-xl outline-none focus:shadow-lg  focus:border-blue-600 "
-              type="text" />
-          </div>
-          <h1 class=" font-semibold text-[23.5px] md:text-[25px] text-slate-700 text-left mt-0 ml-5  ss:ml-4  ">Description</h1>
-          <div class=" flex justify-center " >
-            <input id="description" autofocus="autofocus" defaultValue={note.description}
-              class=" border-2  border-stone-300 mx-auto w-[280px] ss:w-[290px] md:w-[360px] px-2 md:px-4 my-3 py-2.5 rounded-xl outline-none focus:shadow-lg  focus:border-blue-600 "
-              type="text" />
-          </div>
-          <div class=" flex justify-evenly items-center mt-7 ss:mt-10 md:mt-14 mb-2 " >
-            <span onClick={() => handleEdit(note.id)} class="confirmBtn px-9 md:px-11 py-1.5  rounded-xl md:text-[17px]  bg-blue-600 text-white cursor-pointer border-blue-600 border-2 " >Confirm</span>
-            <span onClick={() => handleEdit(note.id)} class="cancelBtn px-9 md:px-12 py-1.5 rounded-xl md:text-[17px]   text-blue-600 bg-white border-blue-400 border-2 cursor-pointer " >Cancel</span>
-          </div>
-        </div>
-      </div>
-    </div >
-  )
-}
-
-
---
+++ src/components/ModalOnEdit.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function ModalOnEdit({ note, setModal, recordEditNote }) {
+    const [title, setTitle] = useState(note.title);
+    const [description, setDescription] = useState(note.description);
+
+
+
+    return (
+        <div className="fixed inset-0 bg-gray-900/50 backdrop-blur-md flex items-center justify-center">
+
+            {/* space out of div */}
+            <div
+                onClick={() => setModal(false)}
+                class="  cursor-pointer  inset-0 absolute z-20 w-full h-screen   "></div>
+
+            <div className=" xl:scale-100 xx:scale-[0.87] mm:scale-[0.85] rr:scale-[0.82] ss:scale-[0.80] bg-white dark:bg-gray-900 p-7 rounded-2xl w-[350px] md:w-[420px] z-50 dark:shadow-indigo-900 ">
+                <h2 className="text-gray-900 dark:text-gray-100 text-center text-2xl font-bold">Edit Todo</h2>
+                <div className="mt-5">
+                    <label className=" text-gray-800 dark:text-gray-200 block font-semibold text-[17px] ">Title</label>
+                    <input
+                        type="text"
+                        className="w-full  py-2.5 px-3 dark:bg-gray-900 border border-stone-300 dark:border-gray-800 dark:text-white mx-auto  mt-3  rounded-[10px] outline-none focus:shadow-lg  focus:border-indigo-600   "
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div className="mt-6">
+                    <label className=" text-gray-800 dark:text-gray-200 block font-semibold text-[17px] ">Description</label>
+                    <input
+                        type="text"
+                        className="w-full  py-2.5 px-3 dark:bg-gray-900 border border-stone-300 dark:border-gray-800 dark:text-white mx-auto  mt-3  rounded-[10px] outline-none focus:shadow-lg  focus:border-indigo-600   "
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                </div>
+                <div className="flex justify-between mt-8">
+                    <button
+                        onClick={() => recordEditNote(note.id, title, description)}
+                        className="bg-gradient-to-tr from-indigo-700 to-indigo-600 text-white px-3 py-1.5 rounded-lg cursor-pointer"
+                    >
+                        Confirm
+                    </button>
+                    <button
+                        onClick={() => setModal(false)}
+                        className="bg-gray-300 px-3 py-1.5 rounded-lg cursor-pointer "
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ModalOnEdit;
